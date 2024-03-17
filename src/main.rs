edbad2d8454f0fee47efae404dfc5568f0816a50
Andre Gabrescht Rossi <andreagr1006@gmail.com>
2024-03-17 19:15:52 -0300

    update: fazendo a listagem dos arquivos caberem melhor no terminal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::fs::{self};
+use std::fs::{self, DirBuilder};
 use clap::Parser;
+use textwrap::{termwidth, fill};
 
 #[derive(Parser)]
 struct Cli {
@@ -19,20 +20,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn list_files(dir: &str, show_all: bool) -> Result<(), Box<dyn std::error::Error>> {
+    let terminal_width = termwidth();
+
+    // Variável auxiliar q verifica o len restante de uma linha do terminal
+    let mut line_length = 0; 
+
     for entry in fs::read_dir(dir)? {
-        let dir_files = entry?;
-        let data = dir_files.metadata()?;
-        if data.is_dir() {
-            if let Ok(name) = dir_files.file_name().into_string() {
-                if !name.starts_with('.') || show_all {
-                    print!("{}/    ", name);
+        let dir_file = entry?;
+        let data = dir_file.metadata()?;
+        let mut name_length = 0;
+
+        if let Ok(name) = dir_file.file_name().into_string() {
+            if !name.starts_with('.') || show_all {
+                if data.is_dir() {
+                    name_length = name.len() + 4;
+                    if name_length + line_length > terminal_width {
+                        println!();
+                        line_length = 0;
+                    }
+                    print!("{}/   ", name);
+                } else {
+                    name_length = name.len() + 3;
+                    if name_length + line_length > terminal_width {
+                        println!();
+                        line_length = 0;
+                    }
+                    print!("{}   ", name);
                 }
             }
-        } else if let Ok(name) = dir_files.file_name().into_string() {
-            if !name.starts_with('.') || show_all {
-                print!("{}   ", name);
-            }
         }
+        line_length += name_length;
     }
 
     Ok(())
