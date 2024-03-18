aabfad824377b76e9e0b9416bdc01c628d036662
Andre Gabrescht Rossi <andreagr1006@gmail.com>
2024-03-18 10:21:31 -0300

    refatoração: criada a função print_file diminuindo a função list_files

--- src/main.rs
+++ src/main.rs
@@ -28,29 +28,36 @@ fn list_files(dir: &str, show_all: bool) -> Result<(), Box<dyn std::error::Error
     for entry in fs::read_dir(dir)? {
         let dir_file = entry?;
         let data = dir_file.metadata()?;
-        let mut name_length = 0;
 
         if let Ok(name) = dir_file.file_name().into_string() {
             if !name.starts_with('.') || show_all {
-                if data.is_dir() {
-                    name_length = name.len() + 4;
-                    if name_length + line_length > terminal_width {
-                        println!();
-                        line_length = 0;
-                    }
-                    print!("{}/   ", name);
-                } else {
-                    name_length = name.len() + 3;
-                    if name_length + line_length > terminal_width {
-                        println!();
-                        line_length = 0;
-                    }
-                    print!("{}   ", name);
-                }
+                line_length = print_file(&name, data.is_dir(), line_length, terminal_width);
             }
         }
-        line_length += name_length;
     }
 
     Ok(())
 } 
+
+fn print_file(file_name: &str, is_dir: bool, mut line_length: usize, terminal_width: usize) -> usize {
+    let mut name_length = 0;
+    if is_dir {
+        name_length = file_name.len() + 4;
+        if name_length + line_length > terminal_width {
+            println!();
+            line_length = 0;
+        }
+        print!("{}/   ", file_name);
+    } else {
+        name_length = file_name.len() + 3;
+        if name_length + line_length > terminal_width {
+            println!();
+            line_length = 0;
+        }
+        print!("{}   ", file_name);
+    }
+
+    line_length += name_length;
+
+    line_length
+}
