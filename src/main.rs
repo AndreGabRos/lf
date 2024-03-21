cbf03f53f617c0870f3ec3e1849b324eb2e7a72d
Andre Gabrescht Rossi <andreagr1006@gmail.com>
2024-03-21 09:14:48 -0300

    update: criando uma tabela para listar os arquivos de forma mais organizada

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::{fs::{self, Metadata}, os::unix::prelude::PermissionsExt};
+use std::{fs::{self, Metadata}, os::unix::prelude::{PermissionsExt, MetadataExt}, ops::AddAssign};
 use clap::Parser;
 use textwrap::termwidth;
+use users::get_user_by_uid;
+
+#[macro_use] extern crate prettytable;
+use prettytable::{Table, Row, Cell, format::{self, TableFormat}};
 
 #[derive(Parser)]
 struct Cli {
@@ -25,47 +29,64 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn get_table_format() -> TableFormat {
+    format::FormatBuilder::new()
+        .column_separator('\t')
+        .borders(' ')
+        .build()
+}
+
 fn list_files(dir: &str, show_all: bool) -> Result<(), Box<dyn std::error::Error>> {
     let terminal_width = termwidth();
 
-    // Variável auxiliar q verifica o len restante de uma linha do terminal
-    let mut line_length = 0; 
+    let mut maior_len: usize = 0;
+    let mut name_files: Vec<String> = Vec::new();
 
     for entry in fs::read_dir(dir)? {
         let dir_file = entry?;
         let data = dir_file.metadata()?;
 
-        if let Ok(name) = dir_file.file_name().into_string() {
+        if let Ok(mut name) = dir_file.file_name().into_string() {
             if !name.starts_with('.') || show_all {
-                line_length = print_file(&name, data.is_dir(), line_length, terminal_width);
+                if data.is_dir() {
+                    name.push('/');
+                }                
+
+                if name.len() > maior_len {
+                    maior_len = name.len();
+                }
+
+                name_files.push(name);
             }
         }
+    }
+    let collums = terminal_width / (maior_len + 7);
+    println!("{collums}");
+    if collums == 0 {
+        for name in name_files {
+            println!("{name}");
+        }
+    } else {
+        print_files_in_table(name_files, collums);
     }
 
     Ok(())
 } 
 
-fn print_file(file_name: &str, is_dir: bool, mut line_length: usize, terminal_width: usize) -> usize {
-    let mut name_length = 0;
-    if is_dir {
-        name_length = file_name.len() + 4;
-        if name_length + line_length > terminal_width {
-            println!();
-            line_length = 0;
+fn print_files_in_table(files_name: Vec<String>, collums: usize) {
+    let mut table = Table::new();
+    table.set_format(get_table_format());
+
+    for names in files_name.chunks(collums) {
+        let mut cells: Vec<Cell> = Vec::new();
+        for name in names {
+            cells.push(Cell::new(name));
         }
-        print!("{}/   ", file_name);
-    } else {
-        name_length = file_name.len() + 3;
-        if name_length + line_length > terminal_width {
-            println!();
-            line_length = 0;
-        }
-        print!("{}   ", file_name);
+        table.add_row(Row::new(cells));
     }
+    let table_text = table.to_string();
 
-    line_length += name_length;
-
-    line_length
+    println!("{}", table_text);
 }
 
 fn get_file_mode(metadata: &Metadata) -> u32 {
@@ -107,15 +128,24 @@ fn turn_mode_into_readable_perm(mode: u32) -> String {
     perm
 }
 
+
 fn list_file_with_metadata(path: &str, show_all: bool) -> Result<(), Box<dyn std::error::Error>> {
     for entry in fs::read_dir(path)? {
         let dir_file = entry?;
+
         let data = dir_file.metadata()?;
+
         let mode = get_file_mode(&data);
         let perm = turn_mode_into_readable_perm(mode);
-        if let Ok(name) = dir_file.file_name().into_string() {
-            if !name.starts_with('.') || show_all {
-                println!("{} {}", perm, name);
+
+        let user_uid = data.uid();
+
+        let user = get_user_by_uid(user_uid).unwrap();
+        let user_name = user.name().to_str().unwrap();
+
+        if let Ok(file_name) = dir_file.file_name().into_string() {
+            if !file_name.starts_with('.') || show_all {
+                println!("{} {} {}", perm, user_name, file_name);
             }
         }
     }
